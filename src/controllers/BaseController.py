9e9e0e62049b742bad6be18da898552361077c75
Aladdin30 <engai3030@gmail.com>
2026-04-25 22:51:39 +0300

    update deals with file upload

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,12 +1,19 @@
 from helpers.config import get_settings,Settings
 import os 
+import random
+import string
 class BaseController:
     
 
     def __init__(self):
         self.app_sttings = get_settings()
+# the frist dirname return the full path for current file
+# the next dirname  return the parent path 
         self.base_dir=os.path.dirname(os.path.dirname(__file__))
         self.files_dir=os.path.join (
             self.base_dir ,
             "assets/files"
             )
+    
+    def generate_random_string(self,length:int=12):
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
--- src/routes/data.py
+++ src/routes/data.py
@@ -5,6 +5,9 @@ from controllers import DataController,ProjectController
 from fastapi.responses import JSONResponse
 from models import ResponseSignal
 import aiofiles
+import logging
+
+logger = logging.getLogger("uvicorn.error")
 data_router=APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1","data"]
@@ -12,9 +15,11 @@ data_router=APIRouter(
 
 @data_router.post("/upload/{project_id}")
 async def upload_data(project_id:str,file:UploadFile,
-                    app_sttings:Settings =Depends(get_settings)):
+                    app_settings:Settings =Depends(get_settings)):
+    
+    data_controller=DataController()
 
-    is_valid,result_signal=DataController().validate_uploaded_file(file=file)
+    is_valid,result_signal=data_controller.validate_uploaded_file(file=file)
     
     if not is_valid:
         return JSONResponse (
@@ -24,13 +29,24 @@ async def upload_data(project_id:str,file:UploadFile,
             }
         ) 
     project_dir_path= ProjectController().get_project_path(project_id=project_id)
-    file_path= os.path.join(
-        project_dir_path,
-        file.filename
+    file_path= data_controller.generate_unique_filename(
+        orig_file_name=file.filename,
+        project_id=project_id
     )
-    async with aiofiles.open(file_path,"wb")as f:
-        while chunk := await file.read(app_sttings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
+    try:
+        async with aiofiles.open(file_path,"wb")as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    
+    except Exception as e:
+        logger.error(f"Error while uploding file: {e}")
+        return JSONResponse (
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        ) 
+        
 
     return JSONResponse(
         content={
